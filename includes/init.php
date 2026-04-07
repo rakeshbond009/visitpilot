ae4a3e04ff123dd21c60cf0a690254acbed01641
Rakesh Verma <rakeshbond009@gmail.com>
2026-04-07 18:37:38 +0530

    Cloud Sync: System Updated

--- includes/init.php
+++ includes/init.php
@@ -3,5 +3,5 @@
  * System Initialization & Version Control
  * Automatically updated on Cloud Sync
  */
-define('APP_VERSION', '2026.04.07.1755');
-define('BUILD_TIMESTAMP', '2026-04-07 17:55:41');
+define('APP_VERSION', '2026.04.07.1837');
+define('BUILD_TIMESTAMP', '2026-04-07 18:37:38');
--- mobile_app/constants.js
+++ mobile_app/constants.js
@@ -1,3 +1,3 @@
 // --- VERSION CONTROL ---
 // This version is manually updated upon releasing updates.
-export const APP_VERSION = "VisitPilot v2.1.4";
+export const APP_VERSION = "VisitPilot v2.1.4"; // CI_VERSION_MARKER
