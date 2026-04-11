0b4098ce1e00560ec198d9ddf8b53c21d6313c0b
Rakesh Verma <rakeshbond009@gmail.com>
2026-04-11 15:52:34 +0530

    Cloud Sync: System Updated

--- includes/init.php
+++ includes/init.php
@@ -3,5 +3,5 @@
  * System Initialization & Version Control
  * Automatically updated on Cloud Sync
  */
-define('APP_VERSION', '2026.04.11.1540');
-define('BUILD_TIMESTAMP', '2026-04-11 15:40:36');
+define('APP_VERSION', '2026.04.11.1552');
+define('BUILD_TIMESTAMP', '2026-04-11 15:52:34');
--- scratch/brute_v1.php
+++ scratch/brute_v1.php
@@ -1,8 +1,11 @@
 <?php
-require 'includes/db.php';
-require 'includes/dahua_helper.php';
+$config = [
+    'client_id' => '2042539358257250304',
+    'client_secret' => 'AhesscxM05NVtR3lYY8auSDKHaWb7AIF',
+    'product_id' => '1539964762',
+    'base_url' => 'https://open-api-sg.dolynkcloud.com'
+];
 
-$config = DahuaHelper::get_config($pdo);
 $appId = $config['client_id'];
 $secret = $config['client_secret'];
 $prodId = $config['product_id'];
@@ -19,6 +22,7 @@ $variants = [
     'F' => $prodId . $appId . $timestamp . $nonce . $secret,
     'G' => $appId . $prodId . $timestamp . $nonce . "v2" . $secret,
     'H' => $appId . $timestamp . $nonce . "v1" . $secret,
+    'I' => $appId . $timestamp . $nonce . "POST\n" . hash('sha256', json_encode(['appId' => $appId, 'appSecret' => $secret]))
 ];
 
 foreach ($variants as $key => $factor) {
