d704a47f79c648e47b48bc7a2032fb3d74b6ae42
Rakesh Verma <rakeshbond009@gmail.com>
2026-04-16 21:47:36 +0530

    Cloud Sync: System Updated

--- includes/dahua_helper.php
+++ includes/dahua_helper.php
@@ -216,11 +216,11 @@ class DahuaHelper
                     'userId' => $dahuaId,
                     'userName' => $visit['visitor_name'],
                     'userType' => 0,
-                    'authorityList' => ['1'],
+                    'authorityList' => ['255'],
                     'permission' => 0,
                     'departmentId' => 0,
                     'verifyType' => 0,
-                    'personalMethod' => 34,
+                    'personalMethod' => 37,
                     'startTime' => date('Y-m-d H:i:s', strtotime('-1 day')),
                     'endTime' => date('Y-m-d H:i:s', strtotime("+" . ($visit['validity_number'] ?: $config['default_validity_number'] ?: '8') . " " . ($visit['validity_unit'] ?: $config['default_validity_unit'] ?: 'hours')))
                 ]
