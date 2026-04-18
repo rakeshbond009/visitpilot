387840627e33bd06f66a5fc49f5bf4808ef3a4c7
Rakesh Verma <rakeshbond009@gmail.com>
2026-04-18 21:50:55 +0530

    Cloud Sync: System Updated

--- mobile_app/utils/config.js
+++ mobile_app/utils/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    API_BASE_URL: 'https://atithi.online/',
+    API_BASE_URL: 'https://visitor.codepilotx.com/',
     //API_BASE_URL: 'http://10.0.2.2/VMS/',
     APP_VERSION: '1.0.0',
 };
