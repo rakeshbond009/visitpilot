240538ca563902826b439244e8e85b857440eea4
Rakesh Verma <rakeshbond009@gmail.com>
2026-07-01 21:24:31 +0530

    Cloud Sync: System Updated

---
+++ sitemap.php
@@ -0,0 +1,31 @@
+<?php
+header('Content-Type: application/xml; charset=utf-8');
+header('X-Robots-Tag: noindex');
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://atithi.online/</loc>
+        <lastmod>2026-07-01</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://atithi.online/privacy.php</loc>
+        <lastmod>2026-07-01</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://atithi.online/terms.php</loc>
+        <lastmod>2026-07-01</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://atithi.online/tenant_login.php</loc>
+        <lastmod>2026-07-01</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.9</priority>
+    </url>
+</urlset>
