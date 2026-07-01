41b784a7c2fac3238ba4202065d6364ccd39bc61
Rakesh Verma <rakeshbond009@gmail.com>
2026-07-01 21:29:13 +0530

    Cloud Sync: System Updated

--- sitemap.php
+++ sitemap.php
@@ -1,6 +1,5 @@
 <?php
 header('Content-Type: application/xml; charset=utf-8');
-header('X-Robots-Tag: noindex');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
